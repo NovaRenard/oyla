77e8dfc8310464c2b93665fa923d6bb356fa7816
saads <sarbas279@gmail.com>
2026-08-02 20:56:27 +0500

    feat: finished project preparation

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -5,11 +5,7 @@ plugins {
 
 android {
     namespace = "kz.oyla.app"
-    compileSdk {
-        version = release(36) {
-            minorApiLevel = 1
-        }
-    }
+    compileSdk = 37
 
     defaultConfig {
         applicationId = "kz.oyla.app"
