f36b842588d56e8778503807d64714b250ff7480
saads <sarbas279@gmail.com>
2026-08-03 10:54:05 +0500

    fix: deploy fixes

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -44,8 +44,8 @@ android {
             )
         }
         release {
-            buildConfigField("String", "API_BASE_URL", "\"https://api.oyla.kz\"")
-            buildConfigField("String", "WS_BASE_URL", "\"wss://api.oyla.kz\"")
+            buildConfigField("String", "API_BASE_URL", "\"https://oyla.koshakan-center.kz\")
+            buildConfigField("String", "WS_BASE_URL", "\"wss://oyla.koshakan-center.kz\"")
             optimization {
                 enable = false
             }
